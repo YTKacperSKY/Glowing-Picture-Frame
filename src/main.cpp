31f09c4f9a85117bd8b607a31a501d0b96e435b1
Kacper <kacper.s@hotmail.de>
2021-11-09 14:20:43 +0100

    added sourecode

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,58 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define LED_PIN 6
+#define LED_COUNT 58
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+
+  strip.begin();
+  strip.show();
+  strip.setBrightness(255);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  rainbow(10);
+}
+
+void setAll(int r, int g, int b) {
+  for(int i=0; i<strip.numPixels(); i++) {
+        
+        strip.setPixelColor(i, r, g, b);
+      }
+}
+
+
+void rainbow(int wait) {
+  while(true) {
+    
+      for(int r = 255; r >= 0; r--) {
+        setAll(r, 255 - r, 0);
+        strip.show(); 
+        delay(wait); 
+      }
+
+      
+      for(int r = 255; r >= 0; r--) {
+        setAll(0, r, 255 - r);
+        strip.show(); 
+        delay(wait);
+      }
+
+      
+      for(int r = 255; r >= 0; r--) {
+        setAll(255 - r, 0, r);
+        strip.show(); 
+        delay(wait);
+      }
+      
+  }
 }
